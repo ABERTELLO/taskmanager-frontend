cef38565d8e1b6ab95dbbea91545e6298aba3731
root <bertelloagustin@gmail.com>
2024-10-09 21:06:05 -0300

    project start

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,13 @@
+// Dependencies
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+
+// Components
 import App from './App';
 
+
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+    render(<App />);
+    const linkElement = screen.getByText(/learn react/i);
+    expect(linkElement).toBeInTheDocument();
 });
